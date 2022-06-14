f7b8c9c590f0c413e86c93ed8c4fec7eb9b32da1
Douglas Lobo <douglasxpt@gmail.com>
2022-06-13 23:11:58 -0300

    Sub feature working

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,17 @@
 import { sum } from "./sum.js";
+import { sub } from "./sub.js";
 
 const btnSum = document.getElementById("sum");
-const spanValue = document.getElementById("value");
+const btnSub = document.getElementById("sub");
+const spanSum = document.getElementById("valueSum");
+const spanSub = document.getElementById("valueSub");
 
 btnSum.addEventListener("click", () => {
-  let value = parseInt(spanValue.innerText);
-  spanValue.innerText = sum(value);
+  let value = parseInt(spanSum.innerText);
+  spanSum.innerText = sum(value);
+});
+
+btnSub.addEventListener("click", () => {
+  let value = parseInt(spanSub.innerText);
+  spanSub.innerText = sub(value);
 });
